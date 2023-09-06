dfd80fd25be3d9068e716123421587f576c99a33
DommSite <johan.johansson06@live.se>
2023-09-06 10:50:53 +0200

    Add project files.

---
+++ uppgift1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
+
+
+
+
+
+
+//(&quot; /\\&quot;);
+//(&quot; / \\&quot;);
+//(&quot; / \&quot;\&quot; \\&quot;);
+//(&quot;/ ______\\&quot;);
